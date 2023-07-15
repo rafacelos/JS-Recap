6ec0415f2284d2f29d3cacbbcecfce550e8d7039
Rafael Vasconcelos <rafa.celos.dev@gmail.com>
2023-07-15 03:00:28 +0100

    Starting Structures

---
+++ main.js
@@ -0,0 +1,25 @@
+const resultScreen = document.querySelector("h2");
+
+const btn1 = document.getElementById(1);
+const btn2 = document.getElementById(2);
+const btn3 = document.getElementById(3);
+const btn4 = document.getElementById(4);
+const btn5 = document.getElementById(5);
+const btn6 = document.getElementById(6);
+const btn7 = document.getElementById(7);
+const btn8 = document.getElementById(8);
+const btn9 = document.getElementById(9);
+
+btn1.addEventListener("click", () => {
+    alert("Hello World!");
+});
+
+btn2.addEventListener("click", myScript);
+btn3.addEventListener("click", myScript);
+btn4.addEventListener("click", myScript);
+btn5.addEventListener("click", myScript);
+btn6.addEventListener("click", myScript);
+btn7.addEventListener("click", myScript);
+btn8.addEventListener("click", myScript);
+btn9.addEventListener("click", myScript);
+
